e8d9d605d7cbc799b2db6ead6991f2ddde7ecffe
Sameer Zaher <sameerzaher@gmail.com>
2021-10-12 12:33:34 +0300

    backend 2

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=360)
+
+class Lesson(models.Model):
+    numOfLesson = models.IntegerField()
+    name = models.CharField(max_length=32)
+    link = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons') 
+    class meta:
+        unique_together = (('numOfLesson', 'course'),) 
+        index_together = (('numOfLesson', 'course'),) 
+
+class UserCourses(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    numOfLesson = models.IntegerField()
+    class meta:
+        unique_together = (('numOfLesson', 'course', 'user'),) 
+        index_together = (('numOfLesson', 'course', 'user'),) 
